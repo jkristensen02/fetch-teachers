37ba81d8bdaf42545b0d8d9923b9dbd2c358e6a9
Julia Kristensen <julia.kristensen05@gmail.com>
2024-05-27 10:08:44 +0200

    fetching teachers

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ window.addEventListener("load", initApp); // When the page is loaded, run initAp
 function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
 }
+async function getTeachers(){
+  const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json");
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+async function getTeachers(){
+  const response = await fetch("https://raw.githubusercontent.com/cederdorff/race/master/data/users.json");
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
